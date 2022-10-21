cbf67140a72fc8d2f3766c35fe1c942a76dfea45
Gustav <sir.gustav.the.coder@gmail.com>
2022-10-21 19:44:42 +0200

    art and some gameplay

--- src/whale.gd
+++ src/whale.gd
@@ -11,34 +11,31 @@ var velocity = Vector2()
 var dashing = false
 var wait_timer = 0
 var playing = true
+var start = true
 
 func _input(event):
 	if event.is_action_pressed("click"):
-		if dashing == false and playing:
+		if dashing == false and (playing or start):
 			target = get_global_mouse_position()
 			dashing = true
+			start = false
 			wait_timer = 0
 			print("daashing")
 
 func _physics_process(delta):
-	velocity = position.direction_to(target) * speed
-	var dist = position.distance_to(target)
-	if dist > 5:
-		look_at(target)
-		$sprite.flip_v = position.x > target.x
-		velocity = move_and_slide(velocity)
-	else:
-		dashing = false
-	
-	if dashing == false:
-		wait_timer += delta
-	
-	if wait_timer > MAX_WAIT and playing:
-		playing = false
-		print("failed")
-		target.y = DEAD_ZONE_TARGET
-	
-	if position.y > DEAD_ZONE:
-		if playing == false:
-			print("died")
-			playing = true
+	if start == false:
+		velocity = position.direction_to(target) * speed
+		var dist = position.distance_to(target)
+		if dist > 5:
+			look_at(target)
+			$sprite.flip_v = position.x > target.x
+			velocity = move_and_slide(velocity)
+		else:
+			dashing = false
+		
+		if dashing == false:
+			wait_timer += delta
+		
+		if wait_timer > MAX_WAIT and playing:
+			playing = false
+			print("failed")
